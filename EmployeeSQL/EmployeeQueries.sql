3e3a5151f43bb09a1fba3a5eb507d6315b436aa9
shoaibfarooqui <110314305+ShoaibFarooqui@users.noreply.github.com>
2023-04-18 23:01:20 -0500

    completed first query

---
+++ EmployeeSQL/EmployeeQueries.sql
@@ -0,0 +1,10 @@
+-- queries for Employees_db
+
+SELECT employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.sex,
+	salaries.salary
+FROM employees
+INNER JOIN salaries ON
+employees.emp_no=salaries.emp_no;
