2b09da572cfb25d7562cf3a7dbd6fcb3dd6f9fa7
shoaibfarooqui <110314305+ShoaibFarooqui@users.noreply.github.com>
2023-04-30 20:28:20 -0500

    Hercules B. and Sales department query

--- EmployeeSQL/EmployeeQueries.sql
+++ EmployeeSQL/EmployeeQueries.sql
@@ -1,3 +1,16 @@
+select * from departments;
+
+select * from dept_emp;
+
+select * from dept_manager;
+
+select * from employees;
+
+select * from salaries;
+
+select * from titles;
+
+
 -- queries for Employees_db
 
 SELECT employees.emp_no,
@@ -28,3 +41,25 @@ inner join departments on
 dept_manager.dept_no = departments.dept_no
 inner join  employees on
 dept_manager.emp_no = employees.emp_no;
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+select employees.first_name,
+	employees.last_name,
+	employees.sex
+from employees
+where first_name = 'Hercules'
+and LEFT(last_name, 1) = 'B';
+
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+
+select departments.dept_no,
+	dept_emp.emp_no,
+	employees.last_name,
+	employees.first_name
+from departments
+inner join dept_emp on
+departments.dept_no = dept_emp.dept_no
+inner join employees on
+dept_emp.emp_no = employees.emp_no
+where departments.dept_name = 'Sales';
